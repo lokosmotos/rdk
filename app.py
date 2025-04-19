87fda9a46afa56dfa34d971d3266bc917e98d6c5
lokosmotos <danial.khalif.dk@gmail.com>
2025-04-19 11:04:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask, render_template, request, send_from_directory, redirect, url_for
+from flask import Flask, render_template, request, send_from_directory
 from utils.excel_to_srt import convert_to_srt
+from utils.profanity_checker import check_and_handle_profanity
 from utils.word_renamer import rename_word_file
-from utils.profanity_checker import check_profanity
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 OUTPUT_FOLDER = 'outputs'
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
@@ -17,29 +18,43 @@ os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 def index():
     return render_template('index.html')
 
-@app.route('/convert', methods=['POST'])
-def convert_route():
-    uploaded_file = request.files['excel']
-    language = request.form.get('language')
+@app.route('/convert', methods=['GET', 'POST'])
+def convert():
+    if request.method == 'POST':
+        uploaded_file = request.files['excel']
+        if not uploaded_file.filename.endswith(('.xlsx', '.xls')):
+            return "Invalid file type. Upload Excel only.", 400
 
-    if not uploaded_file.filename.endswith(('.xlsx', '.xls')):
-        return "Invalid file type. Please upload an Excel file.", 400
+        filepath = os.path.join(UPLOAD_FOLDER, uploaded_file.filename)
+        uploaded_file.save(filepath)
 
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-    uploaded_file.save(filepath)
+        try:
+            srt_path = convert_to_srt(filepath, OUTPUT_FOLDER)
+            return send_from_directory(OUTPUT_FOLDER, os.path.basename(srt_path), as_attachment=True)
+        except Exception as e:
+            return str(e), 500
+    return render_template('convert.html')
 
-    try:
-        srt_path = convert_to_srt(filepath, language, app.config['OUTPUT_FOLDER'])
-    except Exception as e:
-        return str(e), 500
+@app.route('/profanity', methods=['GET', 'POST'])
+def profanity():
+    if request.method == 'POST':
+        file = request.files['file']
+        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(filepath)
 
-    return send_from_directory(app.config['OUTPUT_FOLDER'], os.path.basename(srt_path), as_attachment=True)
+        try:
+            cleaned_path, report = check_and_handle_profanity(filepath, OUTPUT_FOLDER)
+            return render_template('profanity_result.html', report=report, download=cleaned_path)
+        except Exception as e:
+            return str(e), 500
+
+    return render_template('profanity.html')
 
 @app.route('/rename', methods=['GET', 'POST'])
 def rename():
     if request.method == 'POST':
         file = request.files['wordfile']
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(filepath)
 
         try:
@@ -49,21 +64,6 @@ def rename():
             return str(e), 500
 
     return render_template('rename.html')
-
-@app.route('/profanity', methods=['GET', 'POST'])
-def profanity():
-    if request.method == 'POST':
-        file = request.files['file']
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filepath)
-
-        try:
-            results = check_profanity(filepath)
-            return render_template('profanity_result.html', results=results)
-        except Exception as e:
-            return str(e), 500
-
-    return render_template('profanity.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
